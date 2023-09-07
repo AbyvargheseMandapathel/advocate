b08dd037a6216afe44c62d321691f681b6202e22
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-09-07 10:25:24 +0530

    experience Issue fixed

--- accounts/models.py
+++ accounts/models.py
@@ -85,12 +85,6 @@ class LawyerProfile(models.Model):
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} - {self.specialization}"
     
-class Certificate(models.Model):
-    name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='certificates/', blank=True, null=True)
-
-    def __str__(self):
-        return self.name
     
     def calculate_experience(self):
         today = timezone.now().date()
@@ -103,6 +97,15 @@ class Certificate(models.Model):
     def save(self, *args, **kwargs):
         self.experience = self.calculate_experience()
         super().save(*args, **kwargs)
+    
+class Certificate(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='certificates/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    
         
         
 class ContactEntry(models.Model):
