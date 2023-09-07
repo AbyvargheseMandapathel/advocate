5963a9d19b4e22d7317d0d425b91076261c71313
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-09-07 21:34:29 +0530

    lawyers

--- law/urls.py
+++ law/urls.py
@@ -32,8 +32,7 @@ urlpatterns = [
 
     # Add other URL patterns for your project
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
